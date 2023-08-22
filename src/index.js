ebc1ded61b40360041fed2b72a2a4c287c688fca
Zhihare <128489441+Zhihare@users.noreply.github.com>
2023-08-22 10:06:10 +0300

    22.08

--- src/index.js
+++ src/index.js
@@ -16,15 +16,53 @@ const refs = {
 
 refs.form.addEventListener("submit", searhPhoto);
 async function searhPhoto(event) {
-	event.preventDefault();
-	const Photo = refs.input.value;
-	const { hits } = await fetchPix(Photo);
-	console.log(hits);
 
-	refs.gallery.insertAdjacentHTML('beforeend', createMarkup(hits));
-}
+	event.preventDefault();
+	clearPage();
+
+	const Photo = refs.input.value.trim().toLowerCase();
+	// const {
+	// 	elements: { searchQuery },
+	// } = event.target;
+
+	// const search_query = searchQuery.value.trim().toLowerCase();
+
+	if (!Photo) {
+		clearPage();
+		Notiflix.Notify.info('Enter data to search!');
+		return;
+	}
+	try {
+		const { hits, total } = await fetchPix(Photo);
+		console.log(hits);
+		console.log(total);
+
+		if (hits.length === 0) {
+			Notiflix.Notify.failure(
+				`Sorry, there are no images matching your ${Photo}. Please try again.`
+			);
+
+			return;
+		}
+		refs.gallery.insertAdjacentHTML('beforeend', createMarkup(hits));
+		Notiflix.Notify.success(`We found ${total} images.`);
+		lightbox.refresh();
+	} catch (error) {
+		Notify.failure(error.message, 'Something went wrong!');
+
+		clearPage();
+	} finally {
+	}
+};
+
+
 
 const lightbox = new SimpleLightbox('.gallery a', {
 	captionsData: 'alt',
 	captionDelay: 250,
 });
+
+function clearPage() {
+	// fetchPix().this.page = "1";
+	refs.gallery.innerHTML = '';
+}
